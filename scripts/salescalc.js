cfd8676e7b4f26a99c1b58fbecfc14252e4531af
Joseline Venegas <77705338+venegasj@users.noreply.github.com>
2021-02-03 08:47:09 -0700

    Update salescalc.js

--- scripts/salescalc.js
+++ scripts/salescalc.js
@@ -1,14 +1,14 @@
 addEventListener("load", main)
 
 function main() {
-    btCalcSales.addEventListener("click", calculateSales)
+    btCalcSales.addEventListener("click", calculateSales) //main function
 }
 
 function calculateSales() {
-    Price=parseFloat(txPrice.value)
-    TaxRate=parseFloat(txTaxRate.value)/100
-    Tax=Price*TaxRate
-    Total=Price+Tax
+    Price=parseFloat(txPrice.value) // Price formula
+    TaxRate=parseFloat(txTaxRate.value)/100 //TaxRate formula
+    Tax=Price*TaxRate // Tax formula
+    Total=Price+Tax // Total formula
     spTotal.innerHTML=Total.toFixed(2)
 }
 
